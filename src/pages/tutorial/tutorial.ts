e6083a05758f0d5d0b50c16cbb07e90ee6d62a5c
Edycar Reyes <edycarreyes@gmail.com>
2018-11-29 14:36:26 -0600

    se cambian descripciones y se cambian imagenes
    se cambian algunos estilos de imagenes del splashscreen y se hacen cambio en las descripciones del splashscreen

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -32,9 +32,9 @@ export class TutorialPage {
         console.log('Loaded values', values);
         this.slides = [
           {
-            title: values.TUTORIAL_SLIDE1_TITLE,
-            description: values.TUTORIAL_SLIDE1_DESCRIPTION,
-            image: 'assets/img/ica-slidebox-img-1.png',
+            title: 'Bienvenido a NicApp',
+            description: 'Busca, interactua y disfruta con todo lo que te ofrecemos',
+            image: 'assets/icon/Logotipo.png',
           },
           {
             title: values.TUTORIAL_SLIDE2_TITLE,
