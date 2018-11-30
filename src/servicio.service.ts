48fc728f1cd0d9bd3479bc208a032f34cd22a1ca
Edycar Reyes <edycarreyes@gmail.com>
2018-11-29 19:36:48 -0600

    se crea y se comienza a configurar el servicio.service

---
+++ src/servicio.service.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+//import { AngularFireDatabase} from 'angularfire2/database';
+import { MatDialog } from '@angular/material';
