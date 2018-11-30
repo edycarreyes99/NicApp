76233ff50057909338385f7544d79547859f086c
Edycar Reyes <edycarreyes@gmail.com>
2018-11-29 21:09:25 -0600

    se crean los requisitos para el signup
    se crean los requisitos para agregar un usuario nuevo pero aun con errores, se esta trabajando para solucionarlo

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { IonicPage, NavController, ToastController } from 'ionic-angular';
+import { IonicPage, NavController, ToastController, AlertController } from 'ionic-angular';
 
 import { ServicioService } from "../../servicio.service";
 
@@ -16,10 +16,12 @@ export class SignupPage {
   // The account fields for the login form.
   // If you're using the username field with or without email, make
   // sure to add it to the type
-  account: { name: string, email: string, password: string } = {
+  sexo:string;
+  account: { name: string, email: string, password: string, telefono:string} = {
     name: '',
     email: '',
-    password: ''
+    password: '',
+    telefono: ''
   };
 
   // Our translated text strings
@@ -29,7 +31,8 @@ export class SignupPage {
     public user: User,
     public toastCtrl: ToastController,
     public translateService: TranslateService,
-    public servicio: ServicioService
+    public servicio: ServicioService,
+    public alert: AlertController
     ) {
 
     this.translateService.get('SIGNUP_ERROR').subscribe((value) => {
@@ -40,7 +43,28 @@ export class SignupPage {
   doSignup() {
     // Attempt to login in through our User service
 
-
+    if((this.account.email || this.account.password || this.sexo=='Sexo')==''){
+      const alerta = this.alert.create({
+        title: 'Error',
+        subTitle: 'Faltan Campos',
+        message: 'Favor de rellenar todos los campos obligatorios'
+      });
+      alerta.present();
+    }else{
+      this.servicio.registerUser(this.account.email,this.account.password).then((res)=>{
+        this.navCtrl.push(MainPage);
+      }).catch((err)=>{
+        console.log(err);
+        if(err.message=='The email address is badly formatted.'){
+          const alerta = this.alert.create({
+            title: 'Error',
+            subTitle: 'Ha ocurrido un Error',
+            message: 'El Email esta mal Formateado, favor corregirlo'
+          });
+          alerta.present();
+        }
+      });
+    }
     /*this.user.signup(this.account).subscribe((resp) => {
       this.navCtrl.push(MainPage);
     }, (err) => {
